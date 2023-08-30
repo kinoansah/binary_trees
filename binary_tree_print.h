3f7b5dd009f6b38b9e306926fd9ba74d3b9c940e
kinoansah <kinoansah@gmail.com>
2023-08-30 15:21:35 -0700

    New node

---
+++ binary_tree_print.h
@@ -0,0 +1,9 @@
+#ifndef BINARY_TREE_PRINT_H
+#define BINARY_TREE_PRINT_H
+
+#include "binary_trees.h"
+
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* BINARY_TREE_PRINT_H */
+
---
+++ binary_trees.h
@@ -0,0 +1,53 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <math.h>
+
+/* Other necessary includes or typedefs */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/* binary_tree_print.c */
+void binary_tree_print(const binary_tree_t *);
+
+/* Function prototype */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+/* helper functions */
+int _pow_recursion(int x, int y);
+binary_tree_t *bta_helper(binary_tree_t *root, const binary_tree_t *first,
+			  const binary_tree_t *second);
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
+int btib_helper(const binary_tree_t *tree, int low, int hi);
+bst_t *bst_min_val(bst_t *root);
+int btia_helper(const binary_tree_t *tree, int low, int hi);
+int btih_helper(const binary_tree_t *tree);
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
+
+#endif /* BINARY_TREES_H */
